9c41e5aedf80f51e66f842fc1ec969809c662909
Sam Lindley <sam.lindley@chas.se>
2018-09-13 09:55:22 +0200

    small additions

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -34,7 +34,8 @@ module.exports = {
   },
 
   signIn: async (req, res, next) => {
-    console.log("successful log in!!!!");
+    const token = signToken(req.user);
+    res.status(200).json({token});
   },
 
   secret: async (req, res, next) => {
--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -1,21 +1,23 @@
-import express from 'express';
 import usersController from '../controllers/usersController';
 import promiseRouter from 'express-promise-router';
 import {validateBody, schemas} from '../helpers/routeHelpers';
 import passport from 'passport';
 
+// noinspection JSUnusedLocalSymbols
 const passportConf = require('../passport');
 const router = promiseRouter();
+const passportSingIn = passport.authenticate('local', {session: false});
+const passportJWT = passport.authenticate('jwt', {session: false});
 
 
 router.route('/signup')
   .post(validateBody(schemas.authSchema), usersController.signUp);
 
 router.route('/signin')
-  .post(validateBody(schemas.authSchema), passport.authenticate('local', { session: false}), usersController.signIn);
+  .post(validateBody(schemas.authSchema), passportSingIn, usersController.signIn);
 
 router.route('/secret')
-  .get(passport.authenticate('jwt', { session: false }), usersController.secret);
+  .get(passportJWT, usersController.secret);
 
 module.exports = {
   router
